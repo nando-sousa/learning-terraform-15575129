0190609394449642f2ccbc962aec7d7b487586bf
nando-sousa <nando_sousabr@msn.com>
2026-09-19 21:37:40 +1000

    arruma typos

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "10.2.0"
 
-  name    = "mblogy-alb"
+  name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
