855dc538ff2c1f473f2f3e890840401fa5514197
nando-sousa <nando_sousabr@msn.com>
2026-09-19 19:47:40 +1000

    Atualiza AMI para turnkey

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["tomcat-*-prod-eua7ofzpq4eok"]
+    values = ["turnkey-tomcat-*-amd64-marketplace-*-prod-ftp3auyrbkyyq"]
   }
 
   filter {
