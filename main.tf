12de3a814e04c530f7c6400576d9d185c2414fea
nando-sousa <nando_sousabr@msn.com>
2026-09-19 19:02:12 +1000

    Atualiza AMI usada

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["tomcat-*-prod-eua7ofzpq4eok"]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = ["679593333241"] # aws-marketplace
 }
 
 module "blog_vpc" {
