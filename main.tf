d515ccf5525da8cc5a74b1f42c90b2413301f646
nando-sousa <nando_sousabr@msn.com>
2026-09-19 22:03:53 +1000

    nome errado do modulo

--- main.tf
+++ main.tf
@@ -90,11 +90,11 @@ module "blog_autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.blog_vpc.subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
   launch_template_name = "blog"
   security_groups = [module.blog_sg.security_group_id]
   instance_type   = var.instance_type
-
   image_id        = data.aws_ami.app_ami.id
 
   traffic_source_attachments = {
